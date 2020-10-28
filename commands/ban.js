26503ac9ef1829f4e2abb94945eb4844b9374cbd
Lazyy233 <xpierce@hotmail.com>
2020-10-28 21:40:48 +0000

    hjgyuihl

---
+++ commands/ban.js
@@ -0,0 +1,25 @@
+module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission('BAN_MEMBERS')) 
+        message.channel.send("Missing Permissions!");
+    else {
+        let bannedMember = await message.guild.members.ban(args);
+        if(bannedMember){
+
+        try {
+            console.log(bannedMember.tag + " Fue baneado.");
+            message.channel.send (`${bannedMember} has been banned from the server!`)
+        }
+            catch(err) {
+            console.log(err);
+        }
+    }
+}
+}
+
+module.exports.config = {
+    name: "ban",
+    description: "Bans a Users",
+    usage: ",ban",
+    accessableby: "Admins",
+    aliases: ['b']
+}
---
+++ commands/removevip.js
@@ -0,0 +1,15 @@
+module.exports.run = async (bot, message, args) => {
+    let epicRole = message.guild.roles.cache.get('769772710678364162');
+    const member = message.mentions.members.first();
+
+    member.roles.remove(epicRole);
+    message.channel.send('Role Removed')
+}
+
+module.exports.config = {
+    name: "removevip",
+    description: "",
+    usage: ",removevip",
+    accessableby: "Members",
+    aliases: []
+}
