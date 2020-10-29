bb45ff704ae6c008b9892fafd4e792037099c7f5
Lazyy233 <51232781+Lazyy233@users.noreply.github.com>
2020-10-28 17:03:15 -0700

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,3 +1,6 @@
+module.exports = {
+    run: async(client, message, args) => {
+
 if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Invalid Permissions")
 let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
 if (!User) return message.channel.send("Invalid User")
@@ -6,7 +9,8 @@ let banReason = args.join(" ").slice(22);
 if (!banReason) {
   banReason = "None"
 }
-
+    }
+}
 User.ban({reason: banReason})
 
 module.exports.config = {
