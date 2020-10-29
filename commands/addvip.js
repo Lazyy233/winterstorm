1ad121e1464df3064a298b4a008b013fb3bdf280
Lazyy233 <51232781+Lazyy233@users.noreply.github.com>
2020-10-28 17:58:48 -0700

    Update addvip.js

--- commands/addvip.js
+++ commands/addvip.js
@@ -1,6 +1,7 @@
 module.exports.run = async (bot, message, args) => {
     let epicRole = message.guild.roles.cache.get('769772710678364162');
     const member = message.mentions.members.first();
+    message.delete()
 
     member.roles.add(epicRole);
     message.channel.send('Role Added')
